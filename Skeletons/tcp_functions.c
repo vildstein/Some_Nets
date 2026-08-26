7bf955944ce9a13d9caacd54407782e3a2b7f701
vgv <vgv@silentwings.ru>
2026-08-26 19:37:30 +0500

    tcp func little fix

--- Skeletons/tcp_functions.c
+++ Skeletons/tcp_functions.c
@@ -6,6 +6,8 @@
 ERROR_FORWARD_DECL
 SET_ADDRESS_FORWARD_DECL
 
+#define NO_ADDITIONAL_PROTOCOL_REQUIRED 0
+
 SOCKET tcp_client(char* hostName, char* portName) {
 
 	struct sockaddr_in peer;
@@ -23,108 +25,32 @@ SOCKET tcp_client(char* hostName, char* portName) {
 	return sock;
 }
 
-SOCKET tcp_server (char* hostName, char* portName) {
+SOCKET tcp_server(char* hostName, char* portName, struct sockaddr_in* local) {
 
-	struct sockaddr_in local;
-	SOCKET socket;
+	SOCKET sockDecriptor;
 
-	const int on = 1;
+	set_address(hostName, portName, local, TCP_NETWORK_PROTOCOL);
+	sockDecriptor = socket(AF_INET, SOCK_STREAM, NO_ADDITIONAL_PROTOCOL_REQUIRED);
 
-	set_address( hostName, portName, &local, TCP_NETWORK_PROTOCOL );
-
-	socket = ( AF_INET, SOCK_STREAM, 0 );
-
-	if (!IS_VALID_SOCKET( socket )) {
+	if (!IS_VALID_SOCKET( sockDecriptor )) {
 		error( 1, errno, "SOCKET FUNC MISTAKE");
 	}
 
-	if ( setsockopt( socket, SOL_SOCKET, SO_REUSEADDR, (char*) &on, sizeof(on)) ) {
+	const int TURN_SOCK_OPTS_ON = 1;
+	if ( setsockopt(sockDecriptor, SOL_SOCKET, SO_REUSEADDR, &TURN_SOCK_OPTS_ON, sizeof(TURN_SOCK_OPTS_ON)) ) {
 		error( 1, errno, "SET_SOCKET_OTP FUNC MISTAKE");
 	}
 
-	if ( bind ( socket, SA &local, sizeof(local)) ) {
+	int localSize = sizeof(*local);
+	if ( bind( sockDecriptor, SA local, sizeof(*local)) ) {
 		error( 1, errno, "BIND FUNC MISTAKE");
 	}
 
-	if ( listen( socket, NLISTEN) ) {
+	if ( listen( sockDecriptor, NLISTEN) ) {
 		error( 1, errno, "LISTEN FUNC MISTAKE");
 	}
 
-	return socket;
+	return sockDecriptor;
 }
-
-// unsigned long int ntohl, unsigned short int ntol ntohs преобразует двоичные данные из сетевого порядка следования байтов в серверный.
-// https://www.opennet.ru/man.shtml?topic=ntohl&category=3&russian=0
-
-int readn(SOCKET socDescriptor, char* bufferToRead, size_t messageLenght) {
-
-	int count = 0;
-	int recived = 0;
-
-	count = messageLenght;
-
-	while (count > 0) {
-		recived = recv(socDescriptor, bufferToRead,  count, 0);
-
-		if (recived < 0) {
-			if (errno == EINTR) {
-				continue;
-			}
-			return -1;
-		}
-
-		if (recived == 0) {
-			return 	messageLenght - count;
-		}
-
-		bufferToRead += recived;
-		count -= recived;
-
-	}
-
-	return messageLenght;
-}
-
-// Для чтения сообщения переменной длинны
-int readvrec(SOCKET socDescriptor, char* bufferToRead, size_t messageLenght) {
-
-	u_int32_t recLenght = 0;
-	int recived = 0;
-
-	// read record lenght
-	recived = readn(socDescriptor, bufferToRead, messageLenght);
-
-	if (recived != sizeof(u_int32_t)) {
-		return recived < 0 ? -1 : 0;
-	}
-
-	recLenght = ntohl(recLenght);
-	if (recLenght > messageLenght) {
-
-		// Not enought free space in buffer
-		// return errno*/
-		while (recLenght > 0) {
-			recived = readn(socDescriptor, bufferToRead, messageLenght);
-			if (recived != messageLenght) {
-				return recived < 0 ? -1 : 0;
-			}
-			recLenght -= messageLenght;
-			if (recLenght < messageLenght) {
-				messageLenght = recLenght;
-			}
-		}
-		// set_errno(EMSGSIZE);
-		return -1;
-	}
-
-	// read the messgae
-	recived = readn(socDescriptor, bufferToRead, messageLenght);
-	if (recived != recLenght) {
-		return recived < 0 ? -1 : 0;
-	}
-
-	return recLenght;
-}
-
 
 #endif //TCP_FUNCTIONS_H
